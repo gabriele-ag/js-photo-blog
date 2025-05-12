1223a44ed0e1f8a0b2cec842680b8374098374bd
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-12 14:25:23 +0200

    feat: extract resources with axios

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 // console.log("ciao")
 
+const elemCard = document.querySelector("#card-block")
+console.log(elemCard)
+
+
 axios
+    .get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
+        let result = "";
+        console.log(resp.data)
+        for (let i = 0; i < resp.data.length; i++) {
+            const {date, url, title} = resp.data[i]
+            console.log(date, url, title)
+            result += `<div class="card p-xy">
+                            <img src="${url}" alt="img-card">
+                            <p class="card-date">${date}</p>
+                            <h2 class="font-site">${title}</h2>
+                        </div>`
+                
+        }
+        elemCard.innerHTML = result
+        
+    }) 
