20e56b98907ff628cf39d70f1caf13b7976f4d42
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-13 15:14:46 +0200

    feat: created img on click

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 // console.log("ciao")
-
+// Estraggo elementi del DOM
 const elemCard = document.querySelector("#card-block")
-console.log(elemCard)
+const elemOverlay = document.querySelector(".overlay")
 
 
 axios
     .get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
         let result = "";
         console.log(resp.data)
+
+        // Eseguo ciclo per creare le card
         for (let i = 0; i < resp.data.length; i++) {
             const {date, url, title} = resp.data[i]
             console.log(date, url, title)
@@ -18,12 +20,35 @@ axios
                                 <p class="card-date">${date}</p>
                                 <h2 class="font-site card-title">${title}</h2>
                             </div>
-                        </div>`
-        
-                
+                        </div>`     
         }
+
+
         elemCard.innerHTML = result
+
+
+
+        // Estraggo gli elementi del DOM per crearmi l'overlay con immagine
+        const elemDivCard = document.querySelectorAll('.card')
         
-    })
-    
-    
+        elemDivCard.forEach((curDiv, index) => {
+            console.log(curDiv)
+            curDiv.addEventListener('click', (evento) => {
+                evento.preventDefault()
+                elemOverlay.innerHTML = `<button id="btn">Chiudi</button>
+                                         <img class="img-overlay" src="${resp.data[index].url}" alt="img-overlay">`
+                                            
+                                            
+                elemOverlay.style.display = "flex";
+
+                // Aggiungo l'evento per chiudere l'overlay
+            const elemBtn = document.querySelector("#btn")
+            elemBtn.addEventListener("click", (evento) => {
+                evento.preventDefault();
+                elemOverlay.style.display = "none";
+            });
+        })
+
+    });
+      
+});
