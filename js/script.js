aeabeff78713a82b535249b6169fc514be2c3433
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-14 17:45:26 +0200

    feat: Reordered JS code

--- js/script.js
+++ js/script.js
@@ -2,15 +2,32 @@
 const elemCard = document.querySelector("#card-block")
 const elemOverlay = document.querySelector(".overlay")
 
-
 axios
-    .get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
-        let result = "";
-        console.log(resp.data)
+.get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
+    console.log(resp.data)
 
-        // Eseguo ciclo per creare le card
-        for (let i = 0; i < resp.data.length; i++) {
-            const {date, url, title} = resp.data[i]
+    // Dopo aver dichiarato la funzione, creo le card
+    const card = cards(resp.data)
+    elemCard.innerHTML = card
+    
+    // Estraggo gli elementi del DOM per crearmi l'overlay con immagine
+    const elemDivCard = document.querySelectorAll('.card')
+    const overlayUp = overlay(elemDivCard, resp)
+
+    
+    elemOverlay.innerHTML = overlayUp
+    
+})
+        
+
+
+/////// Funzioni ///
+
+// Funzione per la creazione delle card
+function cards(elemento) {
+    let result = '';
+    for (let i = 0; i < elemento.length; i++) {
+            const {date, url, title} = elemento[i]
             console.log(date, url, title)
             result += `<div class="card p-xy">
                             <img class="pin" src="./img/pin.svg" alt="pin-card">
@@ -19,35 +36,36 @@ axios
                                 <p class="card-date">${date}</p>
                                 <h2 class="font-site card-title">${title}</h2>
                             </div>
-                        </div>`     
-        }
+                        </div>`;
+    }
+    return result
+}
 
 
-        elemCard.innerHTML = result
-
-
-
-        // Estraggo gli elementi del DOM per crearmi l'overlay con immagine
-        const elemDivCard = document.querySelectorAll('.card')
-        
-        elemDivCard.forEach((curDiv, index) => {
-            console.log(curDiv)
-            curDiv.addEventListener('click', (evento) => {
-                evento.preventDefault()
-                elemOverlay.innerHTML = `<button id="btn">Chiudi</button>
-                                         <img class="img-overlay" src="${resp.data[index].url}" alt="img-overlay">`
+// Funzione per creazione dell'overlay visualizzabile una volta cliccato sulle card
+function overlay(elemento, resp) {
+    elemento.forEach((curDiv, index) => {
+        console.log(curDiv)
+        curDiv.addEventListener('click', (evento) => {
+            evento.preventDefault()
+            elemOverlay.innerHTML = `<button id="btn">Chiudi</button>
+                                    <img class="img-overlay" src="${resp.data[index].url}" alt="img-overlay">`
                                             
                                             
-                elemOverlay.style.display = "flex";
+            elemOverlay.style.display = "flex";
+             
+             
 
-            // Aggiungo l'evento per chiudere l'overlay
             const elemBtn = document.querySelector("#btn")
-            elemBtn.addEventListener("click", (evento) => {
-                evento.preventDefault();
-                elemOverlay.style.display = "none";
-            });
-        })
+            console.log(elemBtn)
 
-    });
-      
-});
+            elemBtn.addEventListener("click", (evento) => {
+                    evento.preventDefault();
+                    elemOverlay.style.display = "none";
+            })
+            
+        })
+        
+    })
+
+}
